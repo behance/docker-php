f002e1fb82f5702c82d51c09201b5418aa102483
cfortier2 <cfortier2@gmail.com>
2015-03-06 16:58:19 -0500

    add echo statements

--- container/root/run.sh
+++ container/root/run.sh
@@ -2,8 +2,10 @@
 
 if [[ -d /root/.composer ]]
 then
+  echo "Running `composer install`"
   composer install
 else
+  echo "Starting apache2"
   source /etc/apache2/envvars
   exec apache2 -D FOREGROUND
 fi
