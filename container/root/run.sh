6144d7cc55725d452fef66cc5146dc562e834d30
cfortier2 <cfortier2@gmail.com>
2015-03-06 16:48:30 -0500

    run.sh: add composer install if /root/.composer exists

--- container/root/run.sh
+++ container/root/run.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-source /etc/apache2/envvars
-exec apache2 -D FOREGROUND
+
+if [[ -d /root/.composer ]]
+then
+  composer install
+else
+  source /etc/apache2/envvars
+  exec apache2 -D FOREGROUND
+fi
