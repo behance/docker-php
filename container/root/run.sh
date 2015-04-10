f17462a527e795d8894a74bfb3aa24b56502af4c
Jim DeLois <delois@adobe.com>
2015-04-10 19:41:14 -0400

    Promoting Some Apache Env Config Logic

--- container/root/run.sh
+++ container/root/run.sh
@@ -5,7 +5,24 @@ then
   echo "Running `composer install`"
   composer install
 else
-  echo "Starting apache2"
+
+  # Adds docker environment variables where Apache would normally eat them by
+  # entering them as Virtualhost environment variables
+  VARS=`env | grep ^CFG_`;
+  DEST_CONF=/etc/apache2/conf-available/docker-environment.conf
+
+  echo 'Importing Environment Variables'
+  for p in $VARS
+  do
+    ENV='SetEnv '${p/=/ }
+    echo $ENV >> $DEST_CONF
+  done
+
+  echo 'Enabling Docker Config'
+  a2enconf  docker-environment
+
+  echo "Starting Apache 2"
   source /etc/apache2/envvars
   exec apache2 -D FOREGROUND
+
 fi
