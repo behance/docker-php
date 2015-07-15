44db2ef0ade0d3cb47f5eaceecad880a48a0f11f
Bryan Latten <latten@adobe.com>
2015-07-14 22:06:47 -0400

    Clean: bad port from Dockerfile

--- container/root/clean.sh
+++ container/root/clean.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Perform cleanup, ensure unnecessary packages are removed
-RUN apt-get remove -yq \
-        wget \
-        git \
-        php5-dev \
-        gcc && \
-    apt-get autoclean -y && \
-    apt-get autoremove -y && \
-    rm -rf /var/lib/{apt,dpkg,cache,log}/ && \
-    rm -rf /tmp/* /var/tmp/*
+apt-get remove -yq \
+    wget \
+    git \
+    php5-dev \
+    gcc && \
+apt-get autoclean -y && \
+apt-get autoremove -y && \
+rm -rf /var/lib/{apt,dpkg,cache,log}/ && \
+rm -rf /tmp/* /var/tmp/*
