34eb34f52920b4ff6ca25a8ee12c3e5b960d8224
Manny Toledo <mtoledo@adobe.com>
2019-04-12 17:59:32 -0400

    Remove phpinfo() & replace with simple echo for testing
    By default phpinfo can expose credentials in environment
    variables. An unsuspecting developer may accidentally deploy
    without replacing this index.php first. Using `echo` is a safer
    option.

--- container/root/app/public/index.php
+++ container/root/app/public/index.php
@@ -7,17 +7,4 @@ $stderr = fopen( "php://stderr", 'w' );
 fwrite( $stdout, 'Using STDOUT pipe for output' );
 fwrite( $stderr, 'Using STDERR pipe for output' );
 
-phpinfo();
-
-// Use the below form to test uploads
-?>
-
-<form method="post" action="/" enctype="multipart/form-data">
-  <p>
-  Please specify a file, or a set of files:<br>
-  <input type="file" name="datafile">
-  </p>
-  <div>
-  <input type="submit" value="Send">
-  </div>
-</form>
+echo "PHP Version " . PHP_VERSION;
