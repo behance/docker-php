aebaaf7eb341f37cea4170426912c3da679639a2
Bryan Latten <latten@adobe.com>
2021-12-20 16:17:39 -0500

    Test: fixing GH CI test environment
    - Cleaning up testing structure

--- test.sh
+++ test.sh
@@ -14,16 +14,28 @@ set -o pipefail
 MACHINE=$1
 INTERNAL_PORT=8080
 PREFIX="==>"
+BASE_NAME="docker-php"
 
-if [ -z "$1" ]; then
+if [[ -z "$1" ]]; then
     printf "Basic integration script for docker-php and its variants\n\n"
     printf "Usage:\n\ttest.sh [docker-machine ip]\n"
-    exit 1;
+    exit 1
 fi
 
-if [ ! $PHP_VARIANT ]; then
+if [[ ! $PHP_VARIANT ]]; then
   echo "Missing PHP_VARIANT environment variable"
   exit 1
+fi
+
+# Required for dgoss execution, below
+if [[ ! -n "$GOSS_PATH" ]]; then
+  [ -f $(which goss) ]  || { echo 'goss not found, pass GOSS_PATH'; exit 1; }
+  GOSS_PATH=$(which goss)
+fi
+
+if [[ ! -n "$DGOSS_PATH" ]]; then
+  [ -f $(which dgoss) ]  || { echo 'dgoss not found, pass DGOSS_PATH'; exit 1; }
+  DGOSS_PATH=$(which dgoss)
 fi
 
 # Distinguish between naming types
@@ -36,12 +48,11 @@ TEST_STRING="PHP Version ${PHP_VERSION}."
 PLATFORM="${PLATFORM:=linux/amd64}"
 
 # Since containers may or may not be against the same docker engine, create a matrix-unique tag name for outputs
-TAG_NAME="docker-php-${VARIANT_NAME}-${PLATFORM}"
+TAG_NAME="${BASE_NAME}-${VARIANT_NAME}-${PLATFORM}"
 # Formats as lowercase
 TAG_NAME=$(echo $TAG_NAME | tr '[:upper:]' '[:lower:]')
 # Removes slashes
 TAG_NAME=$(echo $TAG_NAME | sed 's/\///')
-
 
 echo "${PREFIX} Variant ${VARIANT_NAME}"
 echo "${PREFIX} PHP Version: ${PHP_VERSION}"
@@ -50,6 +61,9 @@ echo "${PREFIX} Tag ${TAG_NAME}"
 echo "${PREFIX} Platform: ${PLATFORM}"
 
 printf "${PREFIX} Building container\n"
+
+printf "${PREFIX} using goss (${GOSS_PATH})\n"
+printf "${PREFIX} using dgoss (${DGOSS_PATH})\n"
 
 docker buildx build --platform $PLATFORM --iidfile $TAG_NAME -t $TAG_NAME -f $DOCKERFILE_NAME .
 
@@ -62,6 +76,9 @@ BUILD_SHA=$(echo $BUILD_SHA | sed 's/sha256\://')
 printf "${PREFIX} Running container in background\n"
 CONTAINER_ID=$(docker run --rm --platform $PLATFORM --env-file ./.test.env -p $INTERNAL_PORT -d $BUILD_SHA)
 CONTAINER_PORT=$(docker inspect --format '{{ (index (index .NetworkSettings.Ports "8080/tcp") 0).HostPort }}' $CONTAINER_ID)
+
+printf "${PREFIX} Waiting for container to boot\n"
+sleep 5
 
 # ==> Cleanup routine
 # CI environments may be ephemeral, but local environments are not
@@ -77,9 +94,7 @@ function finish {
 
 trap finish EXIT
 
-printf "${PREFIX} Waiting for container to boot\n"
-sleep 5
-
+# -------------------------------------------------------------
 echo "${PREFIX} Check default response, including PHP version identification"
 curl "${MACHINE}:${CONTAINER_PORT}" | grep "${TEST_STRING}"
 
@@ -90,16 +105,19 @@ echo "${PREFIX} Send uploaded file"
 curl --form upload=@tmp.txt "${MACHINE}:${CONTAINER_PORT}" \
   | grep "${TEST_STRING}" > /dev/null
 
+# -------------------------------------------------------------
 echo "${PREFIX} Perform startup tests"
-GOSS_PATH=goss \
-GOSS_SLEEP=5 \
-GOSS_FILES_PATH="runtime-tests/startup/${PHP_VARIANT}/" \
-"${GOSS_INSTALL_PATH}dgoss" run --rm $BUILD_SHA
 
+GOSS_FILES_PATH="runtime-tests/startup/" \
+GOSS_SLEEP=5 \
+$DGOSS_PATH run --rm $BUILD_SHA
+
+# -------------------------------------------------------------
 echo "${PREFIX} Perform NewRelic runtime tests"
-GOSS_PATH=goss \
-GOSS_FILES_PATH="runtime-tests/newrelic/${PHP_VARIANT}/" \
-"${GOSS_INSTALL_PATH}dgoss" run \
+
+GOSS_FILES_PATH="runtime-tests/newrelic/" \
+GOSS_SLEEP=5 \
+$DGOSS_PATH run --rm \
   -e REPLACE_NEWRELIC_APP="abcdefg" \
   -e REPLACE_NEWRELIC_LICENSE="hijklmno" \
   -e NEWRELIC_TRACING_ENABLED="true" \
